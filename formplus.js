9df906459dba78590397656eaf989de58f1859d8
rafael <rafael_gtt@hotmail.com>
2022-08-27 18:27:30 -0300

    [feat] identifica campos obrigatórios e eplica validação no botão Next

--- formplus.js
+++ formplus.js
@@ -41,9 +41,11 @@
 			submit[0].style.display = 'none'; //Ocultando botão de submit
 
 			let formStepsNum = 0;
+			let requiredElements = [];  // Id's dos elementos com preenchimento obrigatório
 
 			// Altera o css das Labels
-			labels.forEach(item => {
+			labels.forEach(item => {				
+				if(item.getAttribute("opts")) requiredElements.push(item.getAttribute("for")); // Captura os id's dos elementos obrigatórios
 				item.style.cssText += 'width: 75%; text-align: left; font-size: 1.6rem; ';
 				item.style.cssText += 'margin: auto; float: none; !important; display: block !important; ';
 			});
@@ -53,6 +55,7 @@
 			const divControlsFiltered = divControls.filter((item) =>  item.parentElement.style.display !== 'none' );
 			const fabrikElementsFiltered = fabrikElements.filter((item) => item.parentElement.parentElement.style.display !== 'none' );
 
+			// Faz a troca de página: se o próximo elemento estiver escondido, esconde o atual e exibe o próximo
 			function updateFormSteps(e) {
 				formElements.forEach(step => {					
 					if(!step.style.cssText.contains('display: none'))
@@ -63,17 +66,40 @@
 			}
 			
 			// Criar botão de próximo
-			function createNextButton() {
+			function createNextButton(item) {				
 				var button = document.createElement("input");
 				button.type = "button";
 				button.value = "Próximo";
 				button.style = 'margin: 5px 10px 0 0; padding: 10px; ';
 				button.className = 'btn btn-primary button';
-				button.addEventListener ("click", function() { 
-					formStepsNum++;
-					updateFormSteps();
+				button.addEventListener ("click", function() {
+					let isValid = true;				
+
+					// Verifica se o elemento atual faz parte dos elementos de preenchimento obrigatório
+					requiredElements.forEach(idRequiredElement => {
+						if(item.querySelector('input[name=' + idRequiredElement + ']'))
+							isValid = checkIsRequired(item.querySelector('input[name=' + idRequiredElement + ']'));
+					});
+
+					// Só passa para a próxima página se o elemento não for obrigatório ou esteja preenchido
+					if(isValid) {
+						formStepsNum++;
+						updateFormSteps();
+						item.querySelector('.fabrikErrorMessage').innerHTML = '';
+					} else {						
+						item.querySelector('.fabrikErrorMessage').innerHTML = 'Campo Obrigatório';						
+					}
 				});
 				return button;
+			}
+
+			// Verifica se o elemento foi preenchido caso não, retorna false e exibe mensagem de erro
+			function checkIsRequired(element) {
+				if(element) {
+					if(element.value === '')
+						return false;
+				}
+				return true;
 			}
 
 			// Criar botão de voltar
@@ -106,14 +132,14 @@
 			})
 
 			// Altera css das div que contém o input
-			divControlsFiltered.forEach((item, index) => {
+			divControlsFiltered.forEach((item, index) => {				
 				var div = document.createElement("div");
 				div.style.cssText = 'margin-top: 30px; ';
 				
 				item.style.cssText += 'margin: 0px auto; width: 75%;';			
 				if(index !== 0) div.appendChild(createPrevButton());; // página do primeiro elemento
 				if(index !== (divControlsFiltered.length - 1)) { // página do ultimo elemento
-					div.appendChild(createNextButton());
+					div.appendChild(createNextButton(item));
 				}
 
 				item.append(div);
